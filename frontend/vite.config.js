6c75849078db2f05c5992ad6c15381b3c5d8f44f
BellaZhukova <izabellaef390@gmail.com>
2025-01-27 11:30:25 +0700

    myapp

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  allowedHosts: [
-    'uapplication-frontend.onrender.com',  // Разрешаем этот хост
-    'localhost',
-  ]
+  server: {
+    port: process.env.PORT || 5173,  // Использовать порт из переменной окружения (PORT), или 5173 как дефолт
+    host: true,                      // Привязать сервер к всем интерфейсам (не только localhost)
+    allowedHosts: [
+      'uapplication-frontend.onrender.com',  // Разрешаем этот хост
+      'localhost',  // Локальный хост для разработки
+    ],
+  }
 })
